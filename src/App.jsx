0b4a4d7c29d5deafa423f989ada9ab18fa244dde
panku-chavan <pankajchavan963@gmail.com>
2025-02-18 10:26:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import ConnectToWallet from "./components/ConnectToWallet";
+
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+function App() {
+  return <>
+  <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <ConnectToWallet/>          
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </>;
+}
+
+export default App;
---
+++ src/components/ConnectToWallet.jsx
@@ -0,0 +1,12 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import React from 'react'
+
+const ConnectToWallet = () => {
+  return (
+    <>
+    <ConnectButton/>
+    </>
+  )
+}
+
+export default ConnectToWallet
